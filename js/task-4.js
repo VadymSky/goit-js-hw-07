bc6df1450d6e00537431605114544f563acecad7
Vadym <innsbruck1999@gmail.com>
2025-07-30 18:29:30 +0100

    modify

--- js/task-4.js
+++ js/task-4.js
@@ -3,7 +3,7 @@ const loginForm = document.querySelector("form.login-form");
 loginForm.addEventListener("submit", event => {
     event.preventDefault();
     const email = loginForm.elements.email.value.trim();;
-    let password = loginForm.elements.password.value.trim();
+    const password = loginForm.elements.password.value.trim();
 
     if (email === "" || password === "") {
         alert('All form fields must be filled in');
